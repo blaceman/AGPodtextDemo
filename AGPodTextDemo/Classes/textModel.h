5d37feba5c0b45e2d7a6110c0f18fb39c81bb767
罗伟贤 <luoweixian@appgame.com>
2016-07-27 16:15:04 +0800

    first commit

---
+++ AGPodTextDemo/Classes/textModel.h
@@ -0,0 +1,13 @@
+//
+//  textModel.h
+//  Pods
+//
+//  Created by new4545 on 16/7/27.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface textModel : NSObject
+
+@end
---
+++ AGPodTextDemo/Classes/textModel.m
@@ -0,0 +1,13 @@
+//
+//  textModel.m
+//  Pods
+//
+//  Created by new4545 on 16/7/27.
+//
+//
+
+#import "textModel.h"
+
+@implementation textModel
+
+@end
